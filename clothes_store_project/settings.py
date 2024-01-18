3776e3244583861c565a97193b84595842582951
noyon-jethitech <noyon.ahammad@jethitech.com>
2024-01-18 18:35:05 -0500

    csrf and hosts updated

--- clothes_store_project/settings.py
+++ clothes_store_project/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x1owqdz9#@#7h52kpeqh)v6mg06##%0w(w%ny65#ua=5mpryt)'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://clothes-store-django.onrender.com/']
 
 # Application definition
 
