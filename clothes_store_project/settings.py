e2c8e7b55fad2afa54044275e95cb66ed163cee9
noyon-jethitech <noyon.ahammad@jethitech.com>
2024-01-18 18:58:52 -0500

    updated2

--- clothes_store_project/settings.py
+++ clothes_store_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://clothes-store-django.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://clothes-store-django.onrender.com']
 
 # Application definition
